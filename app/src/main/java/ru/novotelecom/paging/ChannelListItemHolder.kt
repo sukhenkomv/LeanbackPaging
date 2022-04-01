0c19652a6bd8017876475e3618cb377e27d48cf1
Сухенко Максим Викторович <m.v.sukhenko@novotelecom.ru>
2022-04-01 13:42:31 +0700

    first commit

---
+++ app/src/main/java/ru/novotelecom/paging/ChannelListItemHolder.kt
@@ -0,0 +1,34 @@
+package ru.novotelecom.paging
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.leanback.widget.Presenter
+
+class ChannelListItemHolder : Presenter() {
+
+    override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.channels_list_item, parent, false)
+        return ItemHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any?) {
+        (viewHolder as ItemHolder).bind(item)
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder?) {}
+}
+
+class ItemHolder(itemView: View) : Presenter.ViewHolder(itemView) {
+
+    var titleText: TextView = view.findViewById(R.id.channels_list_item_title_tv)
+
+    fun bind(
+        item: Any?
+    ) {
+        val channelTitle: String = (item as ItemObject).title
+        titleText.text = channelTitle
+    }
+}
---
+++ app/src/main/java/ru/novotelecom/paging/MainActivity.kt
@@ -0,0 +1,58 @@
+package ru.novotelecom.paging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.leanback.app.VerticalGridSupportFragment
+import androidx.leanback.widget.ArrayObjectAdapter
+import androidx.leanback.widget.FocusHighlight
+import androidx.leanback.widget.Presenter
+import androidx.leanback.widget.VerticalGridPresenter
+
+data class ItemObject(
+    val id: Int = 0,
+    val title: String = ""
+)
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var chlistWrapper: FrameLayout
+    lateinit var chlistFragment: VerticalGridSupportFragment
+    lateinit var gridPresenter: VerticalGridPresenter
+    lateinit var itemPresenter: ChannelListItemHolder
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        chlistWrapper = findViewById(R.id.player_activity_channels_list_wrapper_fl)
+
+        chlistFragment =
+            supportFragmentManager.findFragmentById(R.id.player_activity_channels_list_fr) as VerticalGridSupportFragment
+
+        chlistFragment.setOnItemViewClickedListener { itemViewHolder, item, rowViewHolder, row ->
+            if (item != null) {
+            }
+        }
+
+        chlistFragment.setOnItemViewSelectedListener { itemViewHolder, item, rowViewHolder, row ->
+        }
+
+        gridPresenter = VerticalGridPresenter(FocusHighlight.ZOOM_FACTOR_SMALL, true)
+        gridPresenter.numberOfColumns = 1
+        gridPresenter.shadowEnabled = true
+        chlistFragment.gridPresenter = gridPresenter
+
+        itemPresenter = ChannelListItemHolder()
+        val adapter = ArrayObjectAdapter(itemPresenter)
+        for (i in 1..100) {
+            adapter.add(ItemObject(i, "title $i"))
+        }
+//        runOnUiThread {
+        chlistFragment.adapter = adapter
+        chlistFragment.setSelectedPosition(0)
+        chlistWrapper.bringToFront()
+        chlistWrapper.requestFocus()
+//        }
+    }
+}
